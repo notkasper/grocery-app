4c58c8454f3bed1a7eb7f2869f26e81d7c6cb944
Kasper <kasperkarelse98@gmail.com>
2020-05-04 01:51:47 +0200

    order get products

--- src/back-end/controllers/products.js
+++ src/back-end/controllers/products.js
@@ -1,6 +1,9 @@
 const { Product } = require('../models');
 
 exports.getProducts = async (req, res) => {
-  const products = await Product.findAll({ limit: 20 });
+  const products = await Product.findAll({
+    limit: 20,
+    order: [['updatedAt', 'DESC']],
+  });
   res.status(200).send({ data: products });
 };
