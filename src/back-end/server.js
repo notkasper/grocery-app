94889661a062352a6bafe9b776d99046a51e77f1
Kasper <kasperkarelse98@gmail.com>
2020-05-04 00:50:35 +0200

    catch all route

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -51,11 +51,15 @@ const start = async () => {
   app.use(cors());
 
   // Set static folder
-  app.use(express.static(path.join(__dirname, './dist')));
+  app.use(express.static(path.join(__dirname, '/../../dist')));
 
   app.use('/api/v1/products', authMiddleware, products);
   app.use('/api/v1/favorites', authMiddleware, favorites);
   app.use('/api/v1/categories', authMiddleware, categories);
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../../dist/index.html`));
+  });
 
   // eslint-disable-next-line no-unused-vars
   app.use((error, req, res, next) => res.status(500).send({ error }));
