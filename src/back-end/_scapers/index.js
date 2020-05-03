688af8951e9f0a23d8bd0547849bf76e42db633f
Kasper <kasperkarelse98@gmail.com>
2020-05-04 01:48:56 +0200

    front end fixes

--- src/back-end/_scapers/index.js
+++ src/back-end/_scapers/index.js
@@ -1,11 +1,12 @@
-// const scrapeAlbertHeijn = require('./albertHeijn');
+/* eslint-disable no-unused-vars */
+const scrapeAlbertHeijn = require('./albertHeijn');
 const scrapeJumbo = require('./jumbo');
 const models = require('../models');
 
 const run = async () => {
   await models.umzug.up();
-  // await scrapeAlbertHeijn();
-  await scrapeJumbo();
+  await scrapeAlbertHeijn();
+  // await scrapeJumbo();
   models.sequelize.close();
 };
 
