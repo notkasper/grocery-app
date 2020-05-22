5f0510d759ff31d2f16e07d14e41b323de366827
Kasper Karelse <kasperkarelse98@gmail.com>
2020-05-22 19:39:53 +0200

    list page wip

--- services/front-end/src/components/ListPage.jsx
+++ services/front-end/src/components/ListPage.jsx
@@ -1,30 +1,121 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable react/no-array-index-key */
 import React, { useEffect, useState } from 'react';
 import { observer, inject } from 'mobx-react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import Checkmark from '../assets/checkmark.svg';
 
 const Container = styled.div`
   background-color: #f1f6fa;
-  padding: 10px;
+  padding: 1rem;
   grid-gap: 10px;
   padding-bottom: 10000px;
   height: 100vh;
+
+  .item-counter {
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 23px;
+
+    color: #adb5c2;
+  }
 `;
 
 const ListItemContainer = styled.div`
-  background: #ffffff;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
-  border-radius: 8px;
+  margin: 10px 0;
+  display: flex;
+  justify-content: space-between;
 
-  img {
-    min-height: 100px;
+  .image-container {
+    background: #ffffff;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15);
+    border-radius: 8px;
+    max-width: 20vw;
+    display: inline-block;
+    img {
+      max-width: 100%;
+    }
+  }
+
+  .item-details {
+    vertical-align: top;
+    display: inline-block;
+    margin-left: 0.5rem;
+
+    .discount-tag {
+      display: inline-block;
+      background: #44c062;
+      border-radius: 8px;
+      color: #fff;
+      padding: 2px 10px;
+      width: auto;
+    }
+
+    .price-tag {
+      margin: 0.25rem 0;
+      font-weight: 600;
+      color: #44c062;
+      width: auto;
+    }
+
+    .label-tag {
+      font-weight: 500;
+    }
+  }
+
+  .checkbox {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+    appearance: none;
+    border: 2px solid #44c062;
+    border-radius: 8px;
+    background: none;
+    max-height: 2rem;
+    min-width: 2rem;
+  }
+
+  .checkmark {
+    display: inline-block;
+    fill: #adb5c2;
+    max-height: 2rem;
+    min-width: 2rem;
   }
 `;
 
 const ListItem = (props) => {
+  const { image, id } = props;
+  const [checked, setChecked] = useState(false);
+  const history = useHistory();
   return (
     <ListItemContainer>
-      <img alt="product preview" />
+      <div
+        className="image-container"
+        onClick={() => {
+          history.push(`/product/${id}`);
+        }}
+      >
+        <img src={image} alt="product preview" />
+      </div>
+      <div className="item-details">
+        <p className="label-tag">
+          1x Super speciale Gember nuggets met sambal saus
+        </p>
+        <p className="price-tag">€ 0,99</p>
+        <p className="discount-tag">2 voor de prijs van 1</p>
+      </div>
+      {checked ? (
+        <Checkmark className="checkmark" onClick={() => setChecked(false)} />
+      ) : (
+        <input
+          type="checkbox"
+          className="checkbox"
+          onClick={() => setChecked(true)}
+        />
+      )}
     </ListItemContainer>
   );
 };
@@ -38,14 +129,14 @@ const ListPage = inject('applicationStore')(
       setListItems(newListItems);
     };
     useEffect(() => {
-      applicationStore.navbarLabel = 'Lijstje';
+      applicationStore.navbarLabel = 'Boodschappenlijst';
       loadListItems();
     }, []);
     return (
       <Container>
-        <p>Lijst</p>
+        <p className="item-counter">{`${listItems.length} producten in uw lijstje`}</p>
         {listItems.map((item) => (
-          <ListItem key={item.id} />
+          <ListItem key={item.id} id={item.id} image={item.product.image} />
         ))}
       </Container>
     );
