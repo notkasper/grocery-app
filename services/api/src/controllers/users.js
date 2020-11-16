87239b0da625bbd9bb9cac40fada8c5f572b968f
Kasper <kasperkarelse98@gmail.com>
2020-11-16 13:17:42 +0100

    firebase users endpoint

--- services/api/src/controllers/users.js
+++ services/api/src/controllers/users.js
@@ -1,6 +1,23 @@
-const { User } = require('../models');
+/* eslint-disable no-await-in-loop */
+const admin = require('firebase-admin');
+
+const PAGE_SIZE = 1000;
+
+const getUserPage = async (nextPageToken) => {
+  const listUsersResults = await admin
+    .auth()
+    .listUsers(PAGE_SIZE, nextPageToken);
+  const users = listUsersResults.users.map((userRecord) => userRecord.toJSON());
+  return [users, listUsersResults.pageToken];
+};
 
 exports.getUsers = async (req, res) => {
-  const users = await User.findAll();
+  const users = [];
+  let [newUsers, nextPageToken] = await getUserPage();
+  users.push(...newUsers);
+  while (nextPageToken) {
+    [newUsers, nextPageToken] = await getUserPage(nextPageToken);
+    users.push(newUsers);
+  }
   res.status(200).send({ data: users });
 };
