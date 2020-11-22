28d2a51adb70d3585ad86804367266fefddd80e9
Kasper <kasperkarelse98@gmail.com>
2020-11-23 00:56:33 +0100

    improvements for admin product overview

--- services/admin/src/UploadProducts.jsx
+++ services/admin/src/UploadProducts.jsx
@@ -15,6 +15,14 @@ import Typography from '@material-ui/core/Typography';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 import PublishRoundedIcon from '@material-ui/icons/PublishRounded';
 import FilterRoundedIcon from '@material-ui/icons/FilterRounded';
+import CompareRoundedIcon from '@material-ui/icons/CompareRounded';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import { getIdToken, filterDuplicates } from './utils';
 
 const PAGE_SIZE = 100;
@@ -56,6 +64,10 @@ const ConfirmDialog = ({ product, open, handleClose }) => {
 
 const UploadProducts = () => {
   const classes = useStyles();
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(null);
+  const [store, setStore] = useState('jumbo');
+  const [comparison, setComparison] = useState(null);
   const [products, setProducts] = useState([]);
   const [dialogOpen, setDialogOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
@@ -75,15 +87,41 @@ const UploadProducts = () => {
         .send({ products })
         .set('authorization', `Bearer ${idToken}`);
       setProducts([]);
+      setMessage(
+        `${comparison.new_products_count} Products Added. ${comparison.removed_products_count} Products Removed. ${comparison.duplicate_products_count} Products Updated`
+      );
+      setTimeout(() => {
+        setMessage(null);
+      }, 6000);
     } catch (error) {
       console.error(error);
+      setError(error.response.body.error || error.message);
+      setTimeout(() => {
+        setError(null);
+      }, 6000);
+    }
+  };
+  const compareProducts = async () => {
+    try {
+      const idToken = await getIdToken();
+      const response = await request
+        .post('api/v1/products/compare')
+        .send({ products, store })
+        .set('authorization', `Bearer ${idToken}`);
+      setComparison(response.body.data);
+    } catch (error) {
+      console.error(error);
+      setError(error.response.body.error || error.message);
+      setTimeout(() => {
+        setError(null);
+      }, 6000);
     }
   };
   const handleFilter = () => {
     const [uniques, dupes] = filterDuplicates(products);
-    setProducts(uniques);
     console.info(`Uniques found: ${uniques.length}`);
     console.info(`Duplicates found: ${dupes}`);
+    setProducts(uniques);
   };
   const handleDialogClose = () => setDialogOpen(false);
   const columns = [
@@ -115,8 +153,37 @@ const UploadProducts = () => {
 
   return (
     <div>
-      <Grid container>
-        <Grid item xs={2}>
+      <Grid container style={{ marginTop: '2rem' }}>
+        <Grid item xs={12}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">
+              Select Store To Upload Products For
+            </FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={store === 'jumbo'}
+                    onChange={() => setStore('jumbo')}
+                    name="jumbo"
+                  />
+                }
+                label="Jumbo"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={store === 'albert_heijn'}
+                    onChange={() => setStore('albert_heijn')}
+                    name="albert_heijn"
+                  />
+                }
+                label="Albert Heijn"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+        <Grid item xs={3}>
           <input
             className={classes.input}
             id="contained-button-file"
@@ -124,29 +191,39 @@ const UploadProducts = () => {
             onChange={handleFileSelection}
           />
           <label htmlFor="contained-button-file">
-            <Button variant="contained" color="primary" component="span">
-              Upload
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              style={{ float: 'left', marginTop: '2rem' }}
+            >
+              Select File
             </Button>
           </label>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={3}>
           <Button
             variant="contained"
             color="secondary"
             className={classes.button}
             startIcon={<DeleteRoundedIcon />}
-            onClick={() => setProducts([])}
+            onClick={() => {
+              setProducts([]);
+              setComparison(null);
+            }}
+            style={{ float: 'right', marginTop: '2rem' }}
           >
-            Clear
+            Clear Uploaded File
           </Button>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={3}>
           <Button
             variant="contained"
             color="secondary"
             className={classes.button}
             startIcon={<FilterRoundedIcon />}
             onClick={handleFilter}
+            style={{ float: 'right', marginTop: '2rem' }}
           >
             Filter duplicates
           </Button>
@@ -156,12 +233,33 @@ const UploadProducts = () => {
             variant="contained"
             color="primary"
             className={classes.button}
+            startIcon={<CompareRoundedIcon />}
+            onClick={compareProducts}
+            style={{ float: 'right', marginTop: '2rem' }}
+          >
+            Compare Products
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
             startIcon={<PublishRoundedIcon />}
             onClick={uploadProducts}
+            disabled={!comparison}
+            style={{ float: 'right', marginTop: '2rem', marginBottom: '2rem' }}
           >
             Confirm Upload
           </Button>
         </Grid>
+        {comparison && (
+          <Grid item xs={12}>
+            <Typography>{`New products: ${comparison.new_products_count}`}</Typography>
+            <Typography>{`Removed products: ${comparison.removed_products_count}`}</Typography>
+            <Typography>{`Duplicate (Persistent) products: ${comparison.duplicate_products_count}`}</Typography>
+          </Grid>
+        )}
       </Grid>
       <DataGrid
         rows={products}
@@ -176,6 +274,16 @@ const UploadProducts = () => {
         handleDelete={handleDialogClose}
         product={selectedProduct}
       />
+      <Snackbar open={message} autoHideDuration={6000}>
+        <MuiAlert elevation={6} variant="filled" severity="success">
+          {message}
+        </MuiAlert>
+      </Snackbar>
+      <Snackbar open={error} autoHideDuration={6000}>
+        <MuiAlert elevation={6} variant="filled" severity="error">
+          {JSON.stringify(error)}
+        </MuiAlert>
+      </Snackbar>
     </div>
   );
 };
